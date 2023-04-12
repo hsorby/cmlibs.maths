3465282d731355afc86e74da34d99730e64e04c3
Hugh Sorby <h.sorby@auckland.ac.nz>
2023-04-12 15:40:17 +1200

    Transform to stub package to transfer to cmlibs.maths.

--- README.rst
+++ README.rst
@@ -3,12 +3,6 @@
 OpenCMISS Maths
 ===============
 
-OpenCMISS maths functions.  This software can be found on PyPi and installed with the following command::
-
-  pip install opencmiss.maths
-
-Distribution
-============
-
-This software uses regex to extract the version number information from the package. The version number for this package is stored in 'src/opencmiss/maths/__init__.py'
+This package has been renamed. Use :code:`pip install cmlibs.maths` instead.
+New package: https://pypi.org/project/cmlibs.maths/
 
--- setup.py
+++ setup.py
@@ -6,13 +6,6 @@ from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'src', 'opencmiss', 'maths', '__init__.py')) as fd:
-    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-                        fd.read(), re.MULTILINE).group(1)
-
-if not version:
-    raise RuntimeError('Cannot find version information')
-
 
 def readfile(filename, split=False):
     with io.open(filename, encoding="utf-8") as stream:
@@ -31,23 +24,19 @@ readme.append('')
 
 software_licence = readfile("LICENSE")
 
-requires = []
+requires = ["cmlibs.maths"]
 
 setup(
     name='opencmiss.maths',
-    version=version,
+    version="0.2.1",
     description='OpenCMISS Math functions.',
     long_description='\n'.join(readme) + software_licence,
     long_description_content_type='text/x-rst',
-    classifiers=[],
+    classifiers=["Development Status :: 7 - Inactive"],
     author='Hugh Sorby',
     author_email='h.sorby@auckland.ac.nz',
     url='https://github.com/OpenCMISS-Bindings/opencmiss.maths',
     license='Apache Software License',
     license_files=("LICENSE",),
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-    include_package_data=True,
-    zip_safe=False,
     install_requires=requires,
 )
