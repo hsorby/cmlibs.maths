21b0457de067729891bf93e0d7618464e2b38429
Hugh Sorby <h.sorby@auckland.ac.nz>
2025-06-06 14:36:44 +1200

    Remove pkgutil extend_path.

--- src/cmlibs/__init__.py
+++
@@ -1,3 +0,0 @@
-# Namespace package initialisation file.
-from pkgutil import extend_path
-__path__ = extend_path(__path__, __name__)
--- src/cmlibs/maths/__init__.py
+++ src/cmlibs/maths/__init__.py
@@ -1,3 +1,4 @@
 import importlib.metadata
 
 __version__ = importlib.metadata.version("cmlibs.maths")
+print('cmlibs.maths:', __version__)
